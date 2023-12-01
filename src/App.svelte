bc7341c68df34c35edc306795f10ee44a4ddd3e2
Marius Klemp Petersen <mariuskp@stud.ntnu.no>
2023-12-01 09:26:05 +0100

    Complete Christmas-Jeopardy

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<!-- App.svelte -->
+<script>
+  import Board from './components/Board.svelte';
+  import ScoreDisplay from './components/ScoreDisplay.svelte';
+
+  // Key: team name, value: score
+  let teams = {};
+  let currentPoints = 0
+
+  function updateScore(team, points) {
+    teams[team] += points;
+  }
+  function updatePoints(points){
+    currentPoints = points
+}
+
+
+  function addTeam() {
+    const teamName = window.prompt('Enter team name:');
+    if (teamName) {
+      teams[teamName] = 0;
+    }
+  }
+</script>
+
+<main>
+  <Board {updatePoints} {addTeam}/>
+  <ScoreDisplay {teams} {updateScore} {currentPoints}/>
+</main>
+
+<style>
+  /* Add global styling for the app */
+</style>
